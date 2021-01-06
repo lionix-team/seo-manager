be53ea2265e86d76873aa15a5105ee1c81d916ee
Lassad KEFI <lass3ouda@gmail.com>
2021-01-06 13:59:36 +0100

    Update seo-manager.php for Laravel 8.x
    for those who faced the Invalid route action: [Lionix\SeoManager\ImportController] error

--- src/routes/seo-manager.php
+++ src/routes/seo-manager.php
@@ -7,13 +7,13 @@ Route::group([
     'as' => 'seo-manager.',
     'namespace' => 'Lionix\SeoManager'
 ], function () {
-    Route::get('/', 'ManagerController@index')->name('home');
-    Route::get('get-routes', 'ManagerController@getRoutes')->name('get-routes');
-    Route::get('import-routes', 'ImportController')->name('import');
-    Route::get('get-models', 'ManagerController@getModels')->name('get-models');
+    Route::get('/', [ManagerController::class,'index'])->name('home');
+    Route::get('get-routes', [ManagerController::class,'getRoutes'])->name('get-routes');
+    Route::get('import-routes', [ImportController::class])->name('import');
+    Route::get('get-models', [ManagerController::class,'getModels'])->name('get-models');
 
     Route::group(['prefix' => 'locales', 'as' => 'locales.'], function () {
-        Route::get('get-locales', 'LocalesController@getLocales')->name('get');
+        Route::get('get-locales', [LocalesController::class,'getLocales'])->name('get');
     });
 });
 Route::group([
@@ -22,13 +22,13 @@ Route::group([
     'as' => 'seo-manager.',
     'namespace' => 'Lionix\SeoManager'
 ], function () {
-    Route::post('delete-route', 'ManagerController@deleteRoute')->name('delete-route');
-    Route::post('get-model-columns', 'ManagerController@getModelColumns')->name('get-model-columns');
-    Route::post('store-data', 'ManagerController@storeData')->name('store-data');
-    Route::post('get-example-title', 'ManagerController@getExampleTitle')->name('get-example-title');
-    Route::post('sharing-preview', 'ManagerController@sharingPreview')->name('sharing-preview');
+    Route::post('delete-route', [ManagerController::class,'deleteRoute'])->name('delete-route');
+    Route::post('get-model-columns', [ManagerController::class,'getModelColumns'])->name('get-model-columns');
+    Route::post('store-data',[ ManagerController::class,'storeData'])->name('store-data');
+    Route::post('get-example-title', [ManagerController::class,'getExampleTitle'])->name('get-example-title');
+    Route::post('sharing-preview', [ManagerController::class,'sharingPreview'])->name('sharing-preview');
 
     Route::group(['prefix' => 'locales', 'as' => 'locales.'], function () {
-        Route::post('add-locale', 'LocalesController@addLocale')->name('add');
+        Route::post('add-locale', [LocalesController::class,'addLocale'])->name('add');
     });
 });
